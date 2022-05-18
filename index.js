4d7cb94058ee858396470b49841e1bd071023996
Abhishek Chaudhary <abhishekchaudhary1442016@gmail.com>
2022-05-18 14:29:26 +0930

    created project title question

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a title for your project!')
+                return false;
+            }
+        }
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
